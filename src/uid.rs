891ac5a785e2f644aa4791d61e2a26fc2473dbb8
sylv256 <contact@sylv.gay>
2024-11-17 21:00:21 -0500

    feat: Parse Common Handles

--- src/uid.rs
+++ src/uid.rs
@@ -16,6 +16,21 @@ impl Handle {
 			}
 		)
 	}
+
+	pub fn parse_common(handle: String) -> Option<Self> {
+		if handle.chars().nth(0) != Some('@') {
+			return None
+		}
+
+		let handle = &handle[1..=handle.len()];
+		let (user, domain) = handle.split_once("@")?;
+		Some(
+			Self {
+				domain: domain.to_string(),
+				user: user.to_string(),
+			}
+		)
+	}
 }
 
 #[derive(Serialize, Deserialize, Debug)]
