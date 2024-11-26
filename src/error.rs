a11d644253b0962dc6cfe85e44e811ecc5b3f489
sylv256 <contact@sylv.gay>
2024-11-25 19:36:23 -0500

    refactor: simplify error handling

--- src/error.rs
+++ src/error.rs
@@ -32,12 +32,20 @@ impl Into<Error> for ParseError {
 
 pub type ParseResult<T> = core::result::Result<T, ParseError>;
 
+#[derive(Error, IntoStaticStr, Serialize, Debug)]
+#[serde(rename_all = "snake_case")]
+#[strum(serialize_all = "snake_case")]
+pub enum ErrorType {
+	#[error("parse error: {0}")]
+	ParseError(#[from] ParseError)
+}
+
 #[derive(Error, Debug)]
 #[error("{error}: {source}")]
 pub struct Error {
 	pub error: String,
 	#[source]
-	pub source: anyhow::Error,
+	pub source: ErrorType,
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
@@ -48,7 +56,9 @@ impl Serialize for Error {
 	where
 		S: serde::Serializer {
 		let mut state = serializer.serialize_struct("Error", 2)?;
+		let error_type = <&'static str>::from(&self.source);
 		state.serialize_field("error", &self.error)?;
+		state.serialize_field("error_type", error_type)?;
 		state.serialize_field("description", &format!("{}", self.source))?;
 		state.end()
 	}
@@ -61,21 +71,21 @@ impl<'r, 'o: 'r> Responder<'r, 'o> for Error {
 					log::error!("JSON failed to serialize: {:?}", e);
 					Status::InternalServerError
 				})?;
+		let response = rocket::response::content::RawJson(json).respond_to(&request);
 		// todo: make this a macro for every kind of supported error
-		let parse_error = self.source.downcast::<ParseError>();
-		if let Ok(e) = parse_error {
-			let response = rocket::response::content::RawJson(json).respond_to(&request);
-			let err_response = e.respond_to(&request);
-			if let Ok(mut response) = response {
-				if let Ok(err_response) = err_response {
-					response.set_status(err_response.status());
-				}
-				return Ok(response)
-			} else {
-				return response
+		let source = self.source;
+		if let Ok(mut response) = response {
+			match source {
+				ErrorType::ParseError(parse_error) => {
+					let err_response = parse_error.respond_to(&request);
+					if let Ok(err_response) = err_response {
+						response.set_status(err_response.status());
+					}
+					return Ok(response)
+				},
 			}
 		} else {
-			return Err(Status::InternalServerError)
+			return response
 		}
 	}
 }
